ded6ca330332b529fd1e4525309d108a7458ed9d
eric <ericjlee@hotmail.com>
2018-08-30 17:57:00 -0400

    initial production build

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb://eric:abc123@ds125872.mlab.com:25872/mern_learning'
+}
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyparser = require('body-parser');
+const path = require('path');
+
+const items = require('./routes/api/items');
+
+const app = express();
+
+//bodyparser middleware
+app.use(bodyparser.json());
+
+// db configuration
+const db = require('./config/keys').mongoURI;
+
+//connect to database
+mongoose
+    .connect(db)
+    .then(() => console.log('mongodb connection success'))
+    .catch(err => console.log(err));
+
+//use routes
+app.use('/api/items', items);
+
+// serve static assets if we are in production
+
+if(process.env.NODE_ENV === 'production'){
+    //set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+const port = process.env.PORT || 5000;
+
+app.listen(port, () => console.log(`Server started on port ${port}`));
